4954ccb3682e135a20a7eba6f524e2f54945c618
User1 <xumichael94@yahoo.com>
2018-05-20 10:29:48 -0500

    Add webscraper

---
+++ app/src/main/java/io/github/sghsri/representativeapp/CandidateInfo.java
@@ -0,0 +1,69 @@
+package io.github.sghsri.representativeapp;
+import java.util.*;
+
+public class CandidateInfo {
+    private String mName;
+    private Set<Committee> mCommittees = new HashSet<>();
+    private List<String> mEnactedLegislation = new ArrayList<>();
+    private Map<String, Double> mIssues = new HashMap<>();
+
+    public CandidateInfo(String name) {
+        mName = name;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public Set<Committee> getCommittees() {
+        return mCommittees;
+    }
+
+    public Committee findCommittee(String name) {
+        for (Committee c : mCommittees) {
+            if (c.getName().equals(name)) return c;
+        }
+        return null;
+    }
+
+    public List<String> getEnactedLegislation() {
+        return mEnactedLegislation;
+    }
+
+    public void addEnactedLegislation(String legislation) {
+        mEnactedLegislation.add(legislation);
+    }
+
+    public void addCommittee(String name) {
+        mCommittees.add(new Committee(name));
+    }
+
+    public Map<String, Double> getIssues() {
+        return mIssues;
+    }
+
+    public void addIssue(String s, double d) {
+        mIssues.put(s, d);
+    }
+}
+
+class Committee {
+    private String mName;
+    private Map<String, String> mSubcommittees = new HashMap<>();
+
+    Committee(String name) {
+        mName = name;
+    }
+
+    public void addCommittee(String subCom, String position) {
+        mSubcommittees.put(subCom, position);
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public Map<String, String> getSubcommitees() {
+        return mSubcommittees;
+    }
+}
